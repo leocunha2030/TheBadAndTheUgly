5bd1a6dd5b5d76b470dc8d305aa8c395957b3635
Leonardo Cezar Russi Cunha <leocunha20030@hotmail.com>
2024-10-28 15:19:31 -0300

    Código de controle de animações do video
    Não funciona corretamente, vou tentar alterar

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -9,52 +9,83 @@ public class EnemyMove : MonoBehaviour
     private Vector3 target;
 
     public Transform firePoint;
-    public float fireRate;
-    private float fireCount;
+    public float fireRate, waitBetweenShots = 2f, timeToShoot = 1f;
+    private float fireCount, shotwaitCounter, shootTimeCounter;
     public GameObject bullet;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        shootTimeCounter = timeToShoot;
+        shotwaitCounter = waitBetweenShots;
     }
 
     // Update is called once per frame
     void Update()
     {
         target = PlayerMove.instance.transform.position;
-      //target.y = transform.position.y;
 
         agent.destination = target;
 
-        //ansform.LookAt(target);
-        //eRigidbody.linearVelocity = transform.forward * moveSpeed;
-
         if (Vector3.Distance(transform.position, target) > distanceToStop)
         {
+            shootTimeCounter = timeToShoot;
+            shotwaitCounter = waitBetweenShots;
+
             agent.destination = target;
+            animator.SetBool("IsMoving", true);
         }
         else
         {
             agent.destination = transform.position;
+            animator.SetTrigger("Fire");
         }
 
-        fireCount-=Time.deltaTime;
-
-        if(fireCount <= 0)
+        if(shotwaitCounter > 0)
         {
-            fireCount = fireRate;
-            firePoint.LookAt(PlayerMove.instance.transform.position + new Vector3(0f, 1f, 0f));
+            shotwaitCounter -= Time.deltaTime;
 
-            Vector3 targetDirection = PlayerMove.instance.transform.position - transform.position;
-            float angle = Vector3.SignedAngle(targetDirection, transform.forward, Vector3.up);
-
-            if (Mathf.Abs(angle) < 25f)
+            if(shotwaitCounter <= 0)
             {
-                Instantiate(bullet, firePoint.position, firePoint.rotation);
+                shootTimeCounter = timeToShoot;
+            }
+        }
+        else
+        {
+            shootTimeCounter -= Time.deltaTime;
+
+            if(shootTimeCounter > 0)
+            {
+                fireCount -= Time.deltaTime;
+
+                if (fireCount <= 0)
+                {
+                    fireCount = fireRate;
+
+                    firePoint.LookAt(PlayerMove.instance.transform.position + new Vector3(0f, 1f, 0f));
+
+                    Vector3 targetDirection = PlayerMove.instance.transform.position - transform.position;
+                    float angle = Vector3.SignedAngle(targetDirection, transform.forward, Vector3.up);
+
+                    if (Mathf.Abs(angle) < 30f)
+                    {
+                        Instantiate(bullet, firePoint.position, firePoint.rotation);
+                    }
+                    else
+                    {
+                        transform.LookAt(PlayerMove.instance.transform.position);
+                        shotwaitCounter = waitBetweenShots;
+                    }
+
+                }
+
             }
             else
             {
-                agent.destination = target;
+                shotwaitCounter = waitBetweenShots;
             }
         }
+
+       
     }
 }
