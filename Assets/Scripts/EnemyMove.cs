b7e93370a4c97257529ec4c533b44f4258dca41b
Leonardo Cezar Russi Cunha <leocunha20030@hotmail.com>
2024-10-28 16:28:16 -0300

    Espingarda fixada no braço + Animação de tiro + script com delay para o tiro
    Sincronia da animação precisa de melhorias ainda

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -9,92 +9,60 @@ public class EnemyMove : MonoBehaviour
     private Vector3 target;
 
     public Transform firePoint;
-    public float fireRate, waitBetweenShots = 2f, timeToShoot = 1f;
-    private float fireCount, shotwaitCounter, shootTimeCounter;
+    public float fireRate;
+    private float fireCount;
+    public float fireDelay; // Delay do tiro para sincronizar a animação
     public GameObject bullet;
     public Animator animator;
-
-    private bool isFiring = false;
-
     // Start is called before the first frame update
     void Start()
     {
-        shootTimeCounter = timeToShoot;
-        shotwaitCounter = waitBetweenShots;
     }
 
     // Update is called once per frame
     void Update()
     {
         target = PlayerMove.instance.transform.position;
+        //target.y = transform.position.y;
 
         agent.destination = target;
 
+        //ansform.LookAt(target);
+        //eRigidbody.linearVelocity = transform.forward * moveSpeed;
+
         if (Vector3.Distance(transform.position, target) > distanceToStop)
         {
-            shootTimeCounter = timeToShoot;
-            shotwaitCounter = waitBetweenShots;
-
             agent.destination = target;
-            animator.SetBool("IsMoving", true);
-            animator.SetBool("Fire", false);
-            isFiring = false;
         }
         else
         {
             agent.destination = transform.position;
-            animator.SetBool("IsMoving", false);
-            isFiring = true;
         }
 
-        if (shotwaitCounter > 0)
+        fireCount -= Time.deltaTime;
+
+        if (fireCount <= 0)
         {
-            shotwaitCounter -= Time.deltaTime;
+            fireCount = fireRate + fireDelay; // Adiciona o delay do tiro
+            firePoint.LookAt(PlayerMove.instance.transform.position + new Vector3(0f, 1f, 0f));
 
-            if (shotwaitCounter <= 0)
+            Vector3 targetDirection = PlayerMove.instance.transform.position - transform.position;
+            float angle = Vector3.SignedAngle(targetDirection, transform.forward, Vector3.up);
+
+            if (Mathf.Abs(angle) < 45f)
             {
-                shootTimeCounter = timeToShoot;
-            }
-        }
-        else if (isFiring)
-        {
-            shootTimeCounter -= Time.deltaTime;
-
-            if (shootTimeCounter > 0)
-            {
-                fireCount -= Time.deltaTime;
-
-                if (fireCount <= 0)
-                {
-                    fireCount = fireRate;
-
-                    firePoint.LookAt(PlayerMove.instance.transform.position + new Vector3(0f, 1f, 0f));
-
-                    Vector3 targetDirection = PlayerMove.instance.transform.position - transform.position;
-                    float angle = Vector3.SignedAngle(targetDirection, transform.forward, Vector3.up);
-
-                    if (Mathf.Abs(angle) < 30f)
-                    {
-                        Instantiate(bullet, firePoint.position, firePoint.rotation);
-                        animator.SetBool("Fire", true);
-                    }
-                    else
-                    {
-                        transform.LookAt(PlayerMove.instance.transform.position);
-                        shotwaitCounter = waitBetweenShots;
-                    }
-                }
+                Instantiate(bullet, firePoint.position, firePoint.rotation);
+                animator.SetTrigger("Fire");
             }
             else
             {
-                shotwaitCounter = waitBetweenShots;
-                isFiring = false;
-                animator.SetBool("Fire", false);
+                agent.destination = target;
             }
-        }
-        else
-        {
-            animator.SetBool("Fire", false);
+
+            if (agent.remainingDistance < 0.3f)
+            {
+                animator.SetBool("IsMoving", true);
+            }
         }
     }
 }
