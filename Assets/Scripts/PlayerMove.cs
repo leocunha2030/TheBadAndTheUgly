360e39084fff9c9703f9492aba2f04852870698e
Leonardo Cezar Russi Cunha <leocunha20030@hotmail.com>
2024-10-29 12:15:08 -0300

    LeftLeg Rig
    Adicionei o rig da perna esquerda e desabilitei as animações

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -14,17 +14,10 @@ public class PlayerMove : MonoBehaviour
     private Vector3 moveInput;
     private Vector3 velocity;
     public Transform cameraTransform;
-    public Animator animator;
     public bool canJump = true; // Variável para controlar o pulo
 
     public GameObject bullet;
     public Transform firePoint;
-
-    // IK settings
-    public Transform aimTarget;
-    public float aimIKTransitionTime = 0.1f;
-    private float currentIKWeight = 0f; // Peso atual do IK
-    public float aimIKTransitionSpeed = 3f; // Velocidade de transição do IK
 
     public CharacterController characterController;
 
@@ -61,12 +54,7 @@ public class PlayerMove : MonoBehaviour
                 velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
                 jumpVelocity = velocity.y;
                 canJump = false;
-                animator.SetBool("Jumping", true); // Definir o parâmetro bool para a animação de pulo
             }
-        }
-        else
-        {
-            animator.SetBool("Jumping", false);
         }
 
         velocity.y += gravity * Time.deltaTime;
@@ -83,81 +71,24 @@ public class PlayerMove : MonoBehaviour
         newVerticalRotation = Mathf.Clamp(newVerticalRotation, -90f, 90f);
         cameraTransform.localEulerAngles = new Vector3(newVerticalRotation, 0f, 0f);
 
-        // Atualizar o Animator para idle, walk, run e aim
-        if (animator != null)
+        // Shooting
+        if (Input.GetMouseButton(1) && Input.GetMouseButtonDown(0)) // Botão direito e esquerdo do mouse
         {
-            if (moveInput == Vector3.zero)
+            // Código adicionado para ajustar a direção do firePoint
+            RaycastHit hit;
+            if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hit, 200f))
             {
-                animator.SetFloat("Speed", 0); // Idle
-            }
-            else if (!Input.GetKey(KeyCode.LeftShift))
-            {
-                animator.SetFloat("Speed", 0.5f); // Walk
-            }
-            else
-            {
-                animator.SetFloat("Speed", 1); // Run
-            }
-
-            // Definir AimPressed quando o botão direito do mouse for pressionado
-            bool isAiming = Input.GetMouseButton(1); // Botão direito do mouse
-            animator.SetBool("AimPressed", isAiming);
-
-            // Shooting animation
-            if (isAiming && Input.GetMouseButtonDown(0)) // Botão esquerdo do mouse
-            {
-                animator.SetTrigger("Shooting");
-
-                // Código adicionado para ajustar a direção do firePoint
-                RaycastHit hit;
-                if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hit, 200f))
+                if (Vector3.Distance(cameraTransform.position, hit.point) > 1f)
                 {
-                    if (Vector3.Distance(cameraTransform.position, hit.point) > 1f)
-                    {
-                        firePoint.LookAt(hit.point);
-                    }
-                    else
-                    {
-                        firePoint.LookAt(cameraTransform.position + (cameraTransform.forward * 40f));
-                    }
+                    firePoint.LookAt(hit.point);
                 }
-
-                Instantiate(bullet, firePoint.position, firePoint.rotation);
+                else
+                {
+                    firePoint.LookAt(cameraTransform.position + (cameraTransform.forward * 40f));
+                }
             }
-        }
-    }
 
-    // Método IK do Animator
-    void OnAnimatorIK(int layerIndex)
-    {
-        if (animator != null)
-        {
-            if (Input.GetMouseButton(1)) // Botão direito do mouse
-            {
-                // Gradualmente aumente o peso para fazer uma transição suave para o IK
-                currentIKWeight = Mathf.Clamp01(currentIKWeight + Time.deltaTime * aimIKTransitionSpeed);
-
-                // Definir o peso do IK para a mão direita
-                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, currentIKWeight);
-                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, currentIKWeight);
-
-                // Definir a posição e rotação do IK para a mão direita
-                animator.SetIKPosition(AvatarIKGoal.RightHand, aimTarget.position);
-                animator.SetIKRotation(AvatarIKGoal.RightHand, aimTarget.rotation);
-
-                // Definir o peso do IK para a cabeça
-                animator.SetLookAtWeight(currentIKWeight);
-                animator.SetLookAtPosition(aimTarget.position);
-            }
-            else
-            {
-                // Gradualmente reduza o peso do IK para desativar suavemente o IK
-                currentIKWeight = Mathf.Clamp01(currentIKWeight - Time.deltaTime * aimIKTransitionSpeed);
-
-                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, currentIKWeight);
-                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, currentIKWeight);
-                animator.SetLookAtWeight(currentIKWeight);
-            }
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
         }
     }
 }
