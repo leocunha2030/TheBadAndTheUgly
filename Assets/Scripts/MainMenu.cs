e76d55dc91fe3352b60f107a5117a856c89f7b2c
Leonardo Cezar Russi Cunha <leocunha20030@hotmail.com>
2024-11-04 18:18:48 -0300

    Criando Menu Principal

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string firstLevel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
